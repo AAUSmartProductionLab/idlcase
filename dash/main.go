e2121ac152829f68626e19f2c9915da42ca10605
fas <fas@mide.dk>
2020-01-22 14:47:38 +0100

    dash: We are now storing data in influxdb

--- dash/main.go
+++ dash/main.go
@@ -2,19 +2,37 @@ package main
 
 import (
 	"github.com/fasmide/idlcase/dash/gui"
+	"github.com/fasmide/idlcase/dash/sensor"
+	"github.com/fasmide/idlcase/dash/storage"
 	"github.com/fasmide/idlcase/dash/transport"
 )
 
 func main() {
 
+	store, err := storage.NewStore()
+	if err != nil {
+		panic(err)
+	}
+
+	logStoreErrors := func(m sensor.Message) {
+		err := store.Add(m)
+		if err != nil {
+			// panic is maybe quite overkill in this case - we should just
+			// wait and see if its possible to reconnect
+			panic(err)
+		}
+
+	}
+
 	sub := transport.Subscription{
 		Topic: "idl/#",
 		Handlers: []transport.Handler{
 			gui.SensorUpdate,
+			logStoreErrors,
 		},
 	}
 
-	err := sub.Run()
+	err = sub.Run()
 	if err != nil {
 		panic(err)
 	}
---
+++ dash/storage/store.go
@@ -0,0 +1,54 @@
+package storage
+
+import (
+	"fmt"
+
+	"github.com/fasmide/idlcase/dash/sensor"
+
+	client "github.com/influxdata/influxdb1-client/v2"
+)
+
+// Store handles communication with our database
+type Store struct {
+	client.Client
+}
+
+// NewStore returns a ready to go store
+func NewStore() (*Store, error) {
+	c, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr: "http://localhost:8086",
+	})
+	if err != nil {
+		return nil, fmt.Errorf("unable to create influxdb client: %w", err)
+	}
+
+	s := Store{Client: c}
+	return &s, nil
+}
+
+// Add adds a received sensor message for storage
+func (s *Store) Add(m sensor.Message) error {
+	point, err := client.NewPoint(m.Type, map[string]string{
+		"deviceID": m.DeviceID,
+		"unit":     m.Measurement.Unit,
+	}, map[string]interface{}{
+		"value": m.Measurement.Value,
+	}, m.At)
+
+	if err != nil {
+		return fmt.Errorf("unable to create point from Message: %w", err)
+	}
+
+	// Create a new point batch
+	bp, _ := client.NewBatchPoints(client.BatchPointsConfig{
+		Database: "idl",
+	})
+	bp.AddPoint(point)
+
+	err = s.Write(bp)
+	if err != nil {
+		return fmt.Errorf("unable to store data: %w", err)
+	}
+
+	return nil
+}
