a3fd13a5f37477c716a8276910e56f7726a2b8d8
Kristian Mide <fas@mide.dk>
2020-10-29 15:12:22 +0100

    api: last values endpoints finished

--- api/go.mod
+++ api/go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/eclipse/paho.mqtt.golang v1.2.0
 	github.com/influxdata/influxdb-client-go/v2 v2.1.0
+	github.com/influxdata/influxdb1-client v0.0.0-20200827194710-b269163b24ab
 	golang.org/x/net v0.0.0-20201027133719-8eef5233e2a1 // indirect
 )
--- api/main.go
+++ api/main.go
@@ -7,7 +7,8 @@ import (
 
 	"bitbucket.org/ragroup/idlcase/api/api"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
-	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	_ "github.com/influxdata/influxdb1-client" // this is important because of the bug in go mod
+	client "github.com/influxdata/influxdb1-client/v2"
 )
 
 func main() {
@@ -19,8 +20,17 @@ func main() {
 	s := &http.ServeMux{}
 	s.Handle("/forward/", &api.MQTTForwarder{Client: c})
 
+	influx, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr: "http://localhost:8086",
+	})
+
+	if err != nil {
+		fmt.Println("Error creating InfluxDB Client: ", err.Error())
+	}
+	defer influx.Close()
+
 	// /values/current makes room for future /values/xyz endpoints
-	s.Handle("/values/current/", &api.CurrentValues{Client: influxdb2.NewClient("http://localhost:8086", "")})
+	s.Handle("/values/current/", &api.CurrentValues{Client: influx})
 
 	err = http.ListenAndServe(":9090", s)
 
@@ -41,5 +51,3 @@ func connect() (mqtt.Client, error) {
 
 	return client, nil
 }
-
-func dbConnect()
