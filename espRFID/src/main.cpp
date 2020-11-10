7cca99c0fbad0b488413d4c4d99099f28d251f62
Hjalte Nielsen <hjalte33@gmail.com>
2020-11-10 10:03:35 +0100

    major espRFID overhaul

--- espRFID/src/main.cpp
+++ espRFID/src/main.cpp
@@ -38,6 +38,8 @@
 #include "version.h"
 
 /***************************************************************************/
+#include <EasyButton.h>
+
 /***************************************************************************/
 /***************************************************************************/
 
@@ -45,6 +47,12 @@ IDLNetworking idl = IDLNetworking("espRFID", VERSION);
 /*=========================================================================*/
 // RFID reader instance
 MFRC522 mfrc522(CS_PIN,RST_PIN);
+char myTag[30];
+
+
+/*=========================================================================*/
+// easy button instance
+EasyButton button(BTN_PIN,40,false,false);
 
 
 /*=========================================================================*/
@@ -54,44 +62,31 @@ SSD1306Wire display(0x3c, 5, 4);
 
 /**************************************************************************/
 // display loop
-
 void displayLoop() {
     display.clear();
     if (WiFi.status() != WL_CONNECTED) {
-      display.setFont(ArialMT_Plain_24);
-      display.drawString(0, 20, "Connecting...");
+        display.setFont(ArialMT_Plain_24);
+        display.drawString(0, 0, idl.getDeviceId());
+        display.setFont(ArialMT_Plain_10);
+        display.drawString(0, 25, "Connecting...");
+        display.drawString(0, 36, idl.getVersionString());
     } else {
-      display.setFont(ArialMT_Plain_24);
-      display.drawString(0, 0, "deviceId");
-      display.setFont(ArialMT_Plain_10);
-      display.drawString(0, 25, WiFi.localIP().toString());
+        display.setFont(ArialMT_Plain_24);
+        display.drawString(0, 0, idl.getDeviceId());
+        display.setFont(ArialMT_Plain_10);
+        display.drawString(0, 25, WiFi.localIP().toString());
+        display.drawString(0, 36, idl.getVersionString());
     }
 
     display.display();
 }
 
-
-void setup(){
-    Serial.begin(115200);
-
-    // pin setups
-    pinMode(LED_PIN,OUTPUT);
-    pinMode(PIEZO_PIN,OUTPUT);
-    pinMode(BTN_PIN,INPUT);
-    pinMode(IRQ_PIN,INPUT);
-
-    ledcSetup(0,5000,8);
-    ledcAttachPin(PIEZO_PIN,0);
-
-    // Initialising the UI will init the display too.
-    display.init();
-
-    displayLoop(); 
-
-    idl.begin();
-
-    SPI.begin();
-    mfrc522.PCD_Init();
+void displayTag(int showTime = 1000){
+  display.clear();
+  display.setFont(ArialMT_Plain_24);
+  display.drawString(0, 0, myTag);
+  display.display();
+  delay(showTime);
 
 }
 
@@ -108,32 +103,94 @@ void array_to_string(byte array[], unsigned int len, char buffer[])
     buffer[len*2] = '\0';
 }
 
+void openWifiPortal() {
+    // piezo buzzer on of
+    ledcSetup(0,3500,8);
+    ledcWrite(0,50);
+    delay(300);
+    ledcWrite(0,0);
+
+    idl.openWifiPortal();
+    
+    ledcSetup(0,2000,8);
+
+}
+
+void setup(){
+    Serial.begin(115200);
+
+    // pin setups
+    pinMode(LED_PIN,OUTPUT);
+    pinMode(PIEZO_PIN,OUTPUT);
+    pinMode(IRQ_PIN,INPUT);
+
+    ledcSetup(0,2000,8);
+    ledcAttachPin(PIEZO_PIN,0);
+
+    // Initialising the UI will init the display too.
+    display.init();
+
+    displayLoop(); 
+
+    idl.begin();
+
+    SPI.begin();
+    mfrc522.PCD_Init();
+
+    button.begin();
+    button.onPressed( []{displayTag(1500);});
+    button.read();
+    if (button.isPressed()){
+      openWifiPortal();
+    }
+}
+
 void loop() {
 
   idl.loop(0);
 
   displayLoop();
 
+  button.read();
+
   if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) {
     delay(50);
     return;
   }
 
-  // does not work on esp32  :(
-  //tone(PIEZO_PIN,880,100);
+  // only here if card is pressent. 
+
 
   // Dump UID
-  char buff[30];
-  array_to_string(mfrc522.uid.uidByte, mfrc522.uid.size, buff);
-  
-  char buff2[50];
-  sprintf(buff2,"hand held rfid scan of id:%s",buff);
+  array_to_string(mfrc522.uid.uidByte, mfrc522.uid.size, myTag);
 
-  idl.pushEvent("events",buff2, buff);
+
+  char buff2[50];
+  sprintf(buff2,"hand held rfid scan of id:%s",myTag);
+
+  idl.pushEvent("events",buff2, myTag);
   idl.sendEvents();
   Serial.print(F("Card UID:"));
-  Serial.print(buff);
+  Serial.print(myTag);
 
-  delay(500);
+  // piezo buzzer on
+  ledcWrite(0,50);
+  // led on
+  digitalWrite(LED_PIN,HIGH);
+  // display on screen
+  displayTag(0); 
+  // led and piezo time
+  delay(300);
+  // piezo buzzer off
+  ledcWrite(0,0);
+
+  while(button.isPressed()){
+    // keey the display on
+    delay(10);
+    button.read();
+  }
+
+  // led off
+  digitalWrite(LED_PIN,LOW);
   
 }
